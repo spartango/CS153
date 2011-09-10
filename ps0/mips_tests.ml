285db043da411caafcc0885d3a2e29e31d03daa4
spartango@gmail.com <spartango@gmail.com>
2011-09-09 22:38:39 -0400

    made tests cleaner for memory work

--- ps0/mips_tests.ml
+++ ps0/mips_tests.ml
@@ -28,7 +28,7 @@ let test_sw_translate =
     (mk_inst_to_bin_test (Sw(R15, R9, 8l)) 0xad2f0008l)
 
 let test_beq_translate = 
-    (mk_inst_to_bin_test (Beq(R18, R6, 4l)) 0x12460001l)
+    (mk_inst_to_bin_test (Beq(R18, R6, -28l)) 0x1246fff9l)
 
 let test_jr_translate = 
     (mk_inst_to_bin_test (Jr(R21)) 0x02a00008l)
@@ -45,34 +45,31 @@ let instruction_tests = [ test_add_translate;
                           test_jr_translate;
                           test_jal_translate  ]
 
-let test_update_mem = fun () ->
-    let init_state = {m = empty_mem; pc = 0l; r = empty_rf} in
-    let test_inst  = Add(R4, R5, R6)                        in
-    let new_state  = (inst_update_mem test_inst init_state) in 
-    (* We're looking for 0x00A62020 split into 4 bytes *)
-        (  ((mem_lookup 0l new_state.m) = (Byte.mk_byte 0x00l)) 
-        && ((mem_lookup 1l new_state.m) = (Byte.mk_byte 0xa6l))
-        && ((mem_lookup 2l new_state.m) = (Byte.mk_byte 0x20l))
-        && ((mem_lookup 3l new_state.m) = (Byte.mk_byte 0x20l)) )
+let test_update_mem = 
+    (mk_verbose_expect_test 
+	    (fun () ->
+	    let init_state = {m = empty_mem; pc = 0l; r = empty_rf} in
+	    let test_inst  = Add(R4, R5, R6)                        in
+	    let new_state  = (inst_update_mem test_inst init_state) in 
+	    (* We're looking for 0x00A62020 split into 4 bytes *)
+	        ( word_mem_lookup 0l new_state.m ) )
+        0x00A62020l
+        Int32.to_string
+        "Update Memory"
+    )
         
 
 let test_assemble_prog = fun () -> 
-    let test_program    = [ Add(R4, R5, R6);  Add(R4, R5, R6) ] in 
+    let test_program    = [ Add(R4, R5, R6);  Lw(R10, R6, 4l) ] in 
     let new_state = (assem test_program) in 
     (* We're looking for 0x00A62020 split into 4 bytes *)
-        (  ((mem_lookup 0l new_state.m) = (Byte.mk_byte 0x00l)) 
-        && ((mem_lookup 1l new_state.m) = (Byte.mk_byte 0xa6l))
-        && ((mem_lookup 2l new_state.m) = (Byte.mk_byte 0x20l))
-        && ((mem_lookup 3l new_state.m) = (Byte.mk_byte 0x20l))
-        && ((mem_lookup 4l new_state.m) = (Byte.mk_byte 0x00l))
-        && ((mem_lookup 5l new_state.m) = (Byte.mk_byte 0xa6l))
-        && ((mem_lookup 6l new_state.m) = (Byte.mk_byte 0x20l))
-        && ((mem_lookup 7l new_state.m) = (Byte.mk_byte 0x20l)) 
+        (  (word_mem_lookup 0l new_state.m) = 0x00A62020l
+        && (word_mem_lookup 4l new_state.m) = 0x8cca0004l
         && (new_state.pc = 0l) )
 ;;        
 
 (run_test_set instruction_tests "Binary Translation Tests") ;;
 
-(run_test_set [   Test("Update Memory",     test_update_mem); 
-                  Test("Assemble Program",  test_assemble_prog) ] 
+(run_test_set [  test_update_mem; 
+                 Test("Assemble Program",  test_assemble_prog) ] 
               "Assembler Functional Tests" ) 
