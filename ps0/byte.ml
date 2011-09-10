b65dabbff42ebe0133c727987eb79ebaf56fb998
Aaron Watanabe <awatanabe55@gmail.com>
2011-09-10 16:53:48 -0400

    Deleted extraneous code

--- ps0/byte.ml
+++ ps0/byte.ml
@@ -4,9 +4,3 @@ type byte = { b : int32 }
 
 let mk_byte (i : int32) : byte = { b = Int32.logand 0x000000FFl i }
 let b2i32 (b : byte) : int32 = b.b
-
-let my_fun s = 
-    match s with
-        | 1 -> "hi"
-        | 2 -> "bye"
-        | _ -> "goodbye"
