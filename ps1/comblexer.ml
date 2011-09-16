e3b72675976d0c78a428eb6e1ad4397aaac42e30
Aaron Watanabe <awatanabe55@gmail.com>
2011-09-16 13:03:24 -0400

    Augmented token type.

--- ps1/comblexer.ml
+++ ps1/comblexer.ml
@@ -2,7 +2,16 @@ open Lcombinators.GenericParsing
 open Lcombinators.CharParsing
 
 (* the datatype for tokens -- you will need to augment these *)
-type token = INT of int | EOF
+type token = 
+(* Operators *)
+Plus | Minus | Times | Div | Eq | Neq | Lt | Lte | Gt | Gte |
+(* Types *)
+  Int of int | Var of var | Not | And | Or | Assign |  
+(* Control statements *)
+| Seq | If | While | For | Return |
+(* Parens *)
+LParen | RParen | LCurly | RCurly |
+(* End of file *)
 
 exception ImplementMe
 
