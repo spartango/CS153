fba3a1500cad9701252edce33abcf159bf7a6069
spartango@gmail.com <spartango@gmail.com>
2011-09-16 21:23:51 -0400

    fixed some compile errors

--- ps1/comblexer.ml
+++ ps1/comblexer.ml
@@ -3,25 +3,22 @@ open Lcombinators.CharParsing
 
 (* the datatype for tokens -- you will need to augment these *)
 type token = 
-(* Operators *)
-Plus | Minus | Times | Div | Eq | Neq | Lt | Lte | Gt | Gte |
-(* Types *)
-  Int of int | Var of var | Not | And | Or | Assign |  
-(* Control statements *)
-| Seq | If | While | For | Return |
-(* Parens *)
-LParen | RParen | LCurly | RCurly |
-(* End of file *)
+	(* Operators *)
+	Plus | Minus | Times | Div | Eq | Neq | Lt | Lte | Gt | Gte |
+	(* Types *)
+	Int of int | Var of string | Not | And | Or | Assign   
+	(* Control statements *)
+	| Seq | If | While | For | Return |
+	(* Parens *)
+	LParen | RParen | LCurly | RCurly 
 
 exception ImplementMe
 
 (* Parser for alpha_numeric characters *)
-let alpha_num : (char, char) pareser = alt (alpha, dig)
+let alpha_num : (char, char) parser = alt (alpha, dig)
 
 (* Parser for variable *)
-let var = seq(lc_alpha, plus(alt(alpha_num, underscore))
-
-
+let var = seq(lc_alpha, plus(alt(alpha_num, underscore)))
 
 (* the tokenize function -- should convert a list of characters to a list of 
  * Fish tokens using the combinators. *)
