a226f64c5f53291246cc9caddc70a93494813eec
spartango@gmail.com <spartango@gmail.com>
2011-09-19 20:54:36 -0400

    first parser stubs

--- ps1/combparser.ml
+++ ps1/combparser.ml
@@ -4,5 +4,37 @@ open Lcombinators.GenericParsing
 open Comblexer
 open Ast
 
+(* Module for parsing Fish *)
+module FishParsing =
+    struct
+        open GenericParsing
+        
+        (* Statement Parsers *)
+        
+        (* Parser matching If statements              *)
+                                                      
+        (* Parser matching Return statements          *) 
+                                                      
+        (* Parser matching While statements           *) 
+                                                      
+        (* Parser matching For statements             *)
+        
+        (* Parser matching Blocks of statements { x } *) 
+        
+        (* Parser matching Expression statements      *)
+        
+        (* Expression Parsers *) 
+        
+        (* Parameterized Parser for Int [binop] expr *) 
+        
+        (* Parameterized Parser for Var [binop] expr *)
+        
+        (* Parser for Not expr                       *)
+        
+        (* Parser for Paren-contained expression     *)
+                
+end
+        
+
 let rec parse(ts:token list) : program = 
     raise ImplementMe
