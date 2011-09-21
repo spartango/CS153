aa2a7e9125ffddd8e3e6a63b57cdb3b937cb4eb7
Aaron Watanabe <awatanabe55@gmail.com>
2011-09-21 14:05:31 -0400

    Added tokenizer tests.

--- ps1/comblexer.ml
+++ ps1/comblexer.ml
@@ -19,13 +19,16 @@ let tkn2str (t: token) : string =
         | Id(v) -> "Id(" ^ v ^ ")"
         | Int(i) -> string_of_int i
         | Plus -> "+"
+        | Assign -> "="
         | _ -> raise ImplementMe
-
 
 (* Combinators for lexer *)
 
 (* Operators *)
 let plus_combinator = map (fun v -> (Plus)) (c '+')
+
+(* Assignment *)
+let assign_combinator = map (fun _ -> Assign) (c '=')
 
 (* Combinator for variables *)
 let id_combinator = map (fun v -> (Id v)) identifier
@@ -34,7 +37,13 @@ let id_combinator = map (fun v -> (Id v)) identifier
 let int_combinator = map (fun v -> (Int v)) integer
 
 (* Complete combinator *)
-let complete_combinator = alts [id_combinator; int_combinator; plus_combinator]
+let complete_combinator = 
+    map (fun r -> let(_,t) = r in t)
+        (seq(whitespace,
+            (alts [id_combinator; 
+                  int_combinator; 
+                  plus_combinator;
+                  assign_combinator])))
 
 (* the tokenize function -- should convert a list of characters to a list of 
  * Fish tokens using the combinators. *)
@@ -43,12 +52,6 @@ let rec tokenize(cs:char list) : token list =
         | [] -> []
         | _ ->
               match complete_combinator cs with
-                  | Cons((tkn, cs_tail), _) -> tkn::(tokenize cs_tail)
+                  | Cons((tkn, cs_tail), _) -> 
+                            tkn::(tokenize cs_tail)
                   | Nil -> raise LexError
-
-
-    (* Declare combinators *)
-    (* Create massive single combinator with alts *)
-    (* Run combinator over cs *)
-        (* Return token list on end of cs *)
-        (* Handle errors when combinator returns Nil *)
--- ps1/fishcomb_test.ml
+++ ps1/fishcomb_test.ml
@@ -5,28 +5,15 @@ open Lcombinators.CharParsing
 
 let stub = Test("Implemented", (fun () -> false)  )
 
-(* 
-let mk_token_eq_test (t: token) =
-    mk_verbose_expect_test (fun () -> (token_eq t t)) true string_of_bool 
-        ("Equality of " ^ tkn2str(t))
-
-let plus_eq_test = 
-    (mk_token_eq_test Plus);;
-
-let id_eq_test =
-    (mk_token_eq_test (Id "foo"));;
-
-run_test_set [plus_eq_test;
-              id_eq_test] "Token equality tests.";;
-*)
 let lex_test_inputs = [
     (['f';'o';'o'], [(Id "foo")]);
-    (['f';'o';'o';'=';'b';'a';'z'], [(Id "foo"); Eq; (Id "baz")]);
-
+    (['f';'o';'o';'=';'b';'a';'z'], [(Id "foo"); Assign; (Id "baz")]);
     (['5'], [(Int 5)]);
-    (['5';'+';'9'], [(Int 5);Plus;(Int 6)]);
+    (['5';'+';'9'], [(Int 5);Plus;(Int 9)]);
     (['+'], [Plus]);
-    (['+';' ';'f';'o';'o'], [Plus; (Id "foo")])
+    (['+';' ';'f';'o';'o'], [Plus; (Id "foo")]);
+    (['='], [Assign]);
+    (['=';'f';'o';'o'], [Assign; (Id "foo")]);
 ]
 
 let mk_lex_combinator_test (p: (char, token) parser) (expected_token: token)
@@ -62,7 +49,7 @@ let test_plus_combinator =
 
 let test_complete_combinator = 
     let label = "Test for complete combinator" in
-    let test_iterator (errors: string) (case: char list * token list) : string = 
+    let test_case (errors: string) (case: char list * token list) : string = 
         let (cs,tkns) = case in
         let head_token = List.hd tkns in
             match complete_combinator cs with
@@ -73,18 +60,37 @@ let test_complete_combinator =
                           " Lexed: " ^ (tkn2str(tkn))
                 | Nil -> errors ^ "\nReturned no token but expected: " ^
                       (tkn2str(head_token)) in
-    let result = List.fold_left test_iterator "" lex_test_inputs in
+    let result = List.fold_left test_case "" lex_test_inputs in
         if (result <> "")
         then Verbose_Test(result, (fun () -> (false, label)))
         else Verbose_Test("", (fun () -> (true, label)));;
 
+let test_tokenizer_snippets =
+    let label = "Tokenize Fish snippets" in
+    let tkn_list2str (ts: token list) =
+        List.fold_left (fun s t -> s ^ " " ^ (tkn2str t)) "" ts in
+    let test_case (errors: string) (case: char list * token list) : string =
+        let (cs, tkns) = case in
+        let lex = tokenize cs in
+            if lex = tkns
+            then errors
+            else errors ^ "\nExpected:" ^ (tkn_list2str tkns) ^ " Lexed:" ^
+                (tkn_list2str lex) in
+        Verbose_Test(label, 
+                     (fun () -> 
+                          let result =
+                              List.fold_left test_case "" lex_test_inputs in
+                              ((result = ""), (
+                               if result = ""
+                               then "Lexed expected tokens"
+                               else "Lexed tokens did not match expected:" ^ 
+                                   result ^ "\n"))));;
 
-
-run_test_set [stub] "Test Stub";;
 run_test_set [test_id_combinator; 
               test_int_combinator;
               test_plus_combinator;
-              test_complete_combinator ] "Token Combinator Tests"
+              test_complete_combinator ] "Token Combinator Tests";;
+run_test_set [test_tokenizer_snippets] "Tokenizer Tests";;
 
 
 
