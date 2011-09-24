7cfc96950a8704aeef3458935903cc8b68ff0c5b
Aaron Watanabe <awatanabe55@gmail.com>
2011-09-24 19:58:52 -0400

    Changed makefile in order to get fishyacc to compile

--- ps1/fishyacc_test.ml
+++ ps1/fishyacc_test.ml
@@ -1,5 +1,15 @@
 open Test_framework
+open Fishyacc
+open Eval
+
+(* Assumes all test files in test *)
+let mk_file_test (file: string) (expected: int) =
+    mk_verbose_expect_test (fun () -> (eval (parse_code ("/test" ^ file)))) expected string_of_int
+        ("Testing program in " ^ file)
+
+let test_silly =
+    (mk_file_test "silly.fish" 5);;
 
 let stub = Test("Implemented", (fun () -> false)  );;
     
-run_test_set [stub;] "Test Stub"
+run_test_set [test_silly] "Yacc/Lex Tests"
