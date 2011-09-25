cc0111ec4e73fb5fcd70286cb6e4d188d0e95f99
spartango@gmail.com <spartango@gmail.com>
2011-09-25 13:29:04 -0400

    passing more and more tests, specifically, simple expressions

--- ps1/fishcomb_test.ml
+++ ps1/fishcomb_test.ml
@@ -45,8 +45,70 @@ let test_var_alone =
 
 ;;
 
+let test_paren_alone = 
+   Test(     
+        "Parens Simple Test",
+        (fun () ->
+            let input_tokens = 
+                [ (LParen, 0); (Comblexer.Var("x"), 0); (RParen, 0);] in
+            let parsed = (parse_paren_expr input_tokens) in
+            match parsed with 
+            | Cons(((Ast.Var("x"), 0), []), _) -> true
+            | _ -> false
+        )) 
+
+;;
+
+let test_negative_int = 
+   Test(     
+        "Negative Int Test",
+        (fun () ->
+            let input_tokens = 
+                [   (Comblexer.Minus, 0);
+                    (Comblexer.Int(1), 0);
+                ] in
+            let parsed = (parse_int_init input_tokens) in
+            match parsed with 
+            | Cons(((Ast.Int(-1), 0), []), _) -> true
+            | _ -> false
+        )) 
+
+;;
+
+
+let test_int_simple_op = 
+   Test(     
+        "Int 1 + 1 Test",
+        (fun () ->
+            let input_tokens = 
+                [ (Comblexer.Int(1), 0); 
+                  (Comblexer.Plus, 0);
+                  (Comblexer.Int(1), 0);] in
+            let parsed = (parse_int_init input_tokens) in
+            match parsed with 
+            | Cons(
+                    (
+                        ((Ast.Binop(
+                            (Ast.Int(1), 0),
+                            Ast.Plus,
+                            (Ast.Int(1), 0)))
+                        , 0), 
+                     [])
+                  , _ ) -> true
+
+            | _ -> false
+        )
+   )
+
+;;
+    
+
 run_test_set [ test_token_equal;
                test_int_alone;
+               test_negative_int;
                test_var_alone;
-             ]
+               test_paren_alone; 
+            ]
             "Parser Building Blocks"
+;;
+            
