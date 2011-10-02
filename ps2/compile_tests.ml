eb312ed9847d710752a525bade3b6b0ae1968107
spartango@gmail.com <spartango@gmail.com>
2011-10-01 21:16:11 -0400

    pulled in stuff from compile to be able to adequately test it

--- ps2/compile_tests.ml
+++ ps2/compile_tests.ml
@@ -2,8 +2,25 @@ open Test_framework
 open Pretty_print
 open Compile
 
+(* Tests for collecting variables *)
+
+(* Tests for compiling expressions *)
+
+(* Tests for compiling specific statements *)
+
+(* Tests for compiling compound statements *)
+
+
 (* TODO: Implement tests *)
 let stub = Test("Implemented", (fun () -> false)  )
 ;;
 
-run_test_set [stub] "Test Stub";;
+run_test_set [stub] "Collect Var Tests";;
+
+run_test_set [stub] "Compile Expression Tests";;
+
+run_test_set [stub] "Compile Statment Tests";;
+
+run_test_set [stub] "Compile Block Statement Tests";;
+
+
