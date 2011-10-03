5faea1380c436cab24e9c89b5a2c544a4be82cec
spartango@gmail.com <spartango@gmail.com>
2011-10-02 20:36:02 -0400

    constant folder compiles, moving to test

--- ps2/optimize.ml
+++ ps2/optimize.ml
@@ -1,5 +1,6 @@
 open Mips
 open Ast
+open Eval
 
 (* Code Crawling Utilities *)
 
@@ -39,27 +40,43 @@ let thread_jumps (insts : inst list) : inst list =
 ;; 
 
 (* Constant folding: cutting constant-constant ops out of the AST *)
-let rec constant_fold  (statment : Ast.stmt) : Ast.stmt =
+let rec constant_fold  (statement : Ast.stmt) : Ast.stmt =
 	
+	(* Combine and *)
+	let combine_and exp1 exp2 = 
+		let (rexp1, _) = exp1 in
+		let (rexp2, _) = exp2 in
+		match (rexp1, rexp2) with
+		| (Int(i1), Int(i2)) -> Int(bool2int ((i1 = 0) && (i2 = 0)))
+		| _					 -> And(exp1, exp2)
+	in 
+	(* Combine or *)
+	let combine_or exp1 exp2 = 
+		let (rexp1, _) = exp1 in
+		let (rexp2, _) = exp2 in
+		match (rexp1, rexp2) with
+		| (Int(i1), Int(i2)) -> Int(bool2int ((i1 = 0) || (i2 = 0)))
+		| _					 -> Or(exp1, exp2)
+	in 
 	(* Combine binary operations if possible *)
-	let combine_binop exp1 op exp2 =
+	let combine_binop exp1 (op : binop) exp2 =
 		let (rexp1, _) = exp1 in
 		let (rexp2, _) = exp2 in
 		match (rexp1, rexp2) with
 		| (Int(i1), Int(i2)) ->
-			match op with 
+			(match op with 
 			| Plus  -> Int(i1 +  i2)   
 			| Minus -> Int(i1 -  i2)   
 			| Times -> Int(i1 *  i2)   
 			| Div   -> Int(i1 /  i2) 
-			| Eq    -> Int(i1 =  i2)
-			| Neq   -> Int(i1 != i2) 
-			| Lt    -> Int(i1 <  i2)
-			| Lte   -> Int(i1 <= i2) 
-			| Gt    -> Int(i1 >  i2)
-			| Gte   -> Int(i1 >= i2) 
-		| _ -> Binop(exp1, op, exp2)
-
+			| Eq    -> Int(bool2int(i1 =  i2))
+			| Neq   -> Int(bool2int(i1 != i2)) 
+			| Lt    -> Int(bool2int(i1 <  i2))
+			| Lte   -> Int(bool2int(i1 <= i2)) 
+			| Gt    -> Int(bool2int(i1 >  i2))
+			| Gte   -> Int(bool2int(i1 >= i2)))
+		| (_, _) -> Binop(exp1, op, exp2)
+	in
 	(* Handle expressions *)
 	let rec constant_fold_e (expression : Ast.exp) : Ast.exp =
 		let (rexpr, position) = expression in
@@ -69,8 +86,8 @@ let rec constant_fold  (statment : Ast.stmt) : Ast.stmt =
 			| Var(_)                -> rexpr
 			| Binop(exp1, op, exp2) -> (combine_binop (constant_fold_e exp1) op (constant_fold_e exp2))
 			| Not(t_exp) 			-> Not(constant_fold_e t_exp)
-			| And(exp1, exp2)		->
-			| Or(exp1, exp2)		-> 
+			| And(exp1, exp2)		-> (combine_and (constant_fold_e exp1) (constant_fold_e exp2))
+			| Or(exp1, exp2)		-> (combine_or  (constant_fold_e exp1) (constant_fold_e exp2))
 			| Assign(v, t_exp)		-> Assign(v, (constant_fold_e t_exp))
 		in 
 		(folded_expr, position)
