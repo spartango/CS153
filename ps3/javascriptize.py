fd78148c2df374bfd7b270eadbeaa41aafaecce7
spartango@gmail.com <spartango@gmail.com>
2011-10-10 18:23:11 -0400

    added a crosscompiling script to javascript

---
+++ ps3/javascriptize.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import os 
+
+# Reads file, outputting to stdout, and invoking injection on pattern match
+def search_file(filename):
+    # Scope tracking
+    depth  = 0
+    placed = False
+
+    # Pull open file
+    t_file = open(filename, 'r')
+
+    # Line by line read
+    for line in t_file:
+        # Conditional inject
+        if depth == 0 and not placed:
+            # Line check for return
+            print "function ",
+            placed = True
+        
+        if line.find("{") != -1:
+            depth = depth +1 
+            placed = False
+        
+        if line.find("}") != -1:
+            depth = depth -1
+            placed = False;
+                    
+        print line.replace("let ", "var "),
+
+
+if(len(sys.argv) > 1):
+    filename = sys.argv[1]
+    search_file(filename)
+else:
+    print "File not found"
+    exit(1)
---
+++ ps3/xcompile_js.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "[==========] Cross-Compiling Canned Cish Tests To Javascript"
+
+rm -rf compiled_js
+
+mkdir compiled_js
+
+if [[ -e javascriptize.py ]]; then
+    for filename in `ls test/*.cish`; do
+        target=${filename%.cish}
+        output_file="compiled_js/${target:5}.js"
+
+        echo "\x1b\x5b1;36m[ XCOMPILE ]\x1b\x5b0m ${filename:5}"
+        # Tags on the debug print stuff at the header of the program so we can actually test
+
+        # Compile
+        ./javascriptize.py $filename > $output_file
+
+    done 
+else 
+    echo "\x1b\x5b1;31m[  ERROR   ]\x1b\x5b0m: Compiler  not found"
+fi
+
+echo "[==========] Complete"
