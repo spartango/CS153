4ce5010de20d02c585982cdd78e12ab3cabbb4a0
spartango@gmail.com <spartango@gmail.com>
2011-10-18 21:22:43 -0400

    added description of env

--- ps4/scish_compile.ml
+++ ps4/scish_compile.ml
@@ -7,3 +7,15 @@
 exception Unimplemented
 
 let rec compile_exp (e:Scish_ast.exp) : Cish_ast.program = raise Unimplemented
+
+(* Environment *)
+
+(* Environment is a LIFO queue (aka Stack), 
+ * implemented as a linked List.
+ * Each element of the list is a heap-allocated, two-word block
+ * which represents a tuple of the form (value, pointer)
+ * Values in the environment are immutable, instead they can 
+ * only be Shadowed.
+ * Variables are indexed in the environment using DeBruijn indicies
+ * allowing for O(n) lookup by seeking
+*)
