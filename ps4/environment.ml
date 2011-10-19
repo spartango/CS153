8f94119c5b190fbd75c33f22c03e1a8e088e8df5
spartango@gmail.com <spartango@gmail.com>
2011-10-18 22:07:12 -0400

    moving stuff around still...adding lookup

--- ps4/environment.ml
+++ ps4/environment.ml
@@ -1,9 +1,16 @@
 open Cish_ast
 
+let null     = Int(0);;
+let env_name = "env";;
+
 (* Environment *)
 
 (* Environment is a LIFO queue (aka Stack), 
  * implemented as a linked List. *)
+
+(* Initializes an environment for a block of code *)
+let init_env (code : stmt) : stmt =
+  (Let(env_name, null, code), 0)
 
 (*
  * Each element of the list is a heap-allocated, two-word block
@@ -13,3 +20,21 @@ open Cish_ast
  * Variables are indexed in the environment using DeBruijn indicies
  * allowing for O(n) lookup by seeking
 *)
+
+(* Looks up a variable (by index) in the environment *)
+let lookup_env (index : int) : stmt =
+  ( For( ( Assign("i", 
+                  (Int(0), 0) )
+          , 0 ), 
+          ( Binop((Var("i"), 0)
+                  Lt, 
+                  (Int(index), 0) )
+          , 0), 
+          ( Assign("i", 
+                  ( Binop((Var("i"), 0)
+                          Plus, 
+                          (Int(1), 0) )
+                  , 0) )
+          , 0 )
+       )
+  ) 
--- ps4/scish_compile.ml
+++ ps4/scish_compile.ml
@@ -8,27 +8,32 @@ open Environment
 open Cish_ast
 open Scish_ast
 
-let null     = Int(0);;
-let env_name = "env";;
- 
 exception Unimplemented
 
+let result_name = "result";;
+
 let rec compile_exp_r (t_expr : Scish_ast.exp) ( f_list : func list ) 
-                  : (func list, stmt) =
+                      : (func list, stmt) =
+  (* TODO: Implement compilation *)
+  raise Unimplemented
+
+let init_result (code : stmt) : stmt =
+  (Let(result_name, null, code), 0)
 
 (* Initialize an environment    *)
 (* Env starts as a null pointer *)
 let init (t_expr : Scish_ast.exp) : (func list, stmt) =
-  let (fns, code) = compile_exp_r t_expr []                 in
-  let new_code    = (Let(env_name, null, compile_exp_r), 0) in
+  let (fns, code) = compile_exp_r t_expr [] in
+  let new_code    = init_result   code      in
+  let new_code    = init_env      new_code  in
   (fns, new_code)
 
 (* Create a main function       *)
 let rec compile_exp (e:Scish_ast.exp) : Cish_ast.program =
   let (functions, main_body) = (init e
     (Fn( { name = "main"; 
-        args = []; 
-        body = main_body; 
-        pos  = 0;
+           args = []; 
+           body = main_body; 
+           pos  = 0;
        } ) 
      @ functions)
