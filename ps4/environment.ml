96db18c694c7258845c6a6517093f4f541853dd4
spartango@gmail.com <spartango@gmail.com>
2011-10-18 21:26:18 -0400

    Pulling env into its own nice file

---
+++ ps4/environment.ml
@@ -0,0 +1,15 @@
+(* Environment *)
+
+(* Environment is a LIFO queue (aka Stack), 
+ * implemented as a linked List. *)
+
+(* Initialize an environment *)
+
+(*
+ * Each element of the list is a heap-allocated, two-word block
+ * which represents a tuple of the form (value, pointer)
+ * Values in the environment are immutable, instead they can 
+ * only be Shadowed.
+ * Variables are indexed in the environment using DeBruijn indicies
+ * allowing for O(n) lookup by seeking
+*)
