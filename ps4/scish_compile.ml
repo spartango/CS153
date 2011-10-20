66fb2c68bbe83d3c703f06febd98ac7a8ac47e46
Aaron Watanabe <awatanabe55@gmail.com>
2011-10-20 13:40:56 -0400

    First commit

--- ps4/scish_compile.ml
+++ ps4/scish_compile.ml
@@ -6,4 +6,24 @@
 
 exception Unimplemented
 
-let rec compile_exp (e:Scish_ast.exp) : Cish_ast.program = raise Unimplemented
+type exp = 
+  Int of int                    (* integer constants *)
+| Var of var                    (* read value in variable *)
+| PrimApp of primop * exp list  (* apply primitive operation *)
+| Lambda of var * exp           (* an anonymous function *)
+| App of exp * exp              (* call a function *)
+| If of exp * exp * exp         (* if e1 != 0 then e2 else e3 *)
+
+let rec compile_exp (e:Scish_ast.exp) : Cish_ast.program = 
+    let dummy_pos = 0 in
+    match e with
+        | Int i -> (Cish_ast.Int i, dummy_pos)
+        | Var v -> (Cish_ast.Var v, dummy_pos)
+        | PrimApp (op, exps) -> raise Unimplemented
+        | Lambda (v, e) -> raise Unimplemented
+        | App (e1, e2) -> raise Unimplemented
+        | If(e1, e2, e3) -> raise Unimplemented
+(* Match against Scish_epxressions*)
+              (*Int -> Cish int *)
+              (*Var -> Cish var *)
+              (*PrimApp (op, exps) -> Handle operators *)
