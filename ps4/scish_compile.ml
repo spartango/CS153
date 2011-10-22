37ee24c35a4dee8d15c11667f7da77f27336bfc3
spartango@gmail.com <spartango@gmail.com>
2011-10-22 14:56:16 -0400

    merge effects

--- ps4/scish_compile.ml
+++ ps4/scish_compile.ml
@@ -15,15 +15,11 @@ let label_counter = ref 0
 let new_int()   = (label_counter := (!label_counter) + 1; !label_counter)
 let new_temp() = 
     "temp_var" ^ (string_of_int (new_int()))
+let new_function() = "f" ^ (string_of_int (new_int()))
 
-let dummy_pos = 0;;
 let result_name = "result";;
 let result_var : Cish_ast.exp = (Cish_ast.Var(result_name), stub_pos)
 
-(* generate fresh labels *)
-let label_counter = ref 0
-let new_int() = (label_counter := (!label_counter) + 1; !label_counter)
-let new_function() = "f" ^ (string_of_int (new_int()))
 
 let create_closure (arg : string) 
                    (body : exp) 
@@ -63,24 +59,7 @@ let rec compile_exp_r ( t_expr : Scish_ast.exp )
                           let code = lookup_env scope_loc in 
                          (* Env lookup   *)
                           (f_list, scope, code)
-  | PrimApp(op, exps) -> 
-        (* Maps over expression list, compiling expressions while accumulating function list and scope *)
-        let(f_list1, scope1, stmts) = List.fold_left
-            (fun (t_f_list, t_scope, t_stmts) t_e ->
-                 let (n_f_list, n_scope, n_stmt) = compile_exp_r t_e t_f_list t_scope in
-                     (n_f_list, n_scope, n_stmt::stmts))
-            (f_list, scope, []) exps in
-        let assign value = (f_list1, scope1, Cish_ast.Exp(Cish_ast.Assign(result_var, value), stub_pos)) in
-            match op with
-                | Plus   assign Cish_ast.Binop(
-                | Minus  (* subtract two ints *)
-                | Times  (* multiply two ints *)
-                | Div    (* divide two ints *)
-                | Cons   (* create a pair *)
-                | Fst    (* fetch the 1st component of a pair *)
-                | Snd    (* fetch the 2nd component of a pair *)
-                | Eq     (* compare two ints for equality *)
-                | Lt     (* compare two ints for inequality *)
+  | PrimApp(op, exps) -> raise Unimplemented
   | Lambda(v, t_exp)  -> create_closure v t_expr f_list scope 
   | App(e1, e2)       -> (* Compile e2 *)
                          (* Push result on to stack *)
