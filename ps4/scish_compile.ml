a88d5bc32c147b6247cb45df4fdf39da5ef22fe7
Aaron Watanabe <awatanabe55@gmail.com>
2011-10-22 14:39:11 -0400

    Helpers for PrimApp

--- ps4/scish_compile.ml
+++ ps4/scish_compile.ml
@@ -11,8 +11,9 @@ open Scish_ast
 
 exception Unimplemented
 
-let result_name = "result";;
 let dummy_pos = 0;;
+let result_name = "result";;
+let result_var : Cish_ast.exp = (Cish_ast.Var(result_name), dummy_pos)
 
 let rec compile_exp_r ( t_expr : Scish_ast.exp ) 
                       ( f_list : func list     ) 
@@ -22,10 +23,31 @@ let rec compile_exp_r ( t_expr : Scish_ast.exp )
   | Int(i)            -> (f_list, var_list,
                           (Cish_ast.Exp(Cish_ast.Int i, dummy_pos), dummy_pos))
   | Var(v)            -> raise Unimplemented (* TODO: do lookup      *)
-  | PrimApp(op, exps) -> raise Unimplemented (* TODO: Apply          *)
+  | PrimApp(op, exps) -> 
+        (* Maps over expression list, compiling expressions while accumulating function list and scope *)
+        let(f_list1, scope1, stmts) = List.fold_left
+            (fun (t_f_list, t_scope, t_stmts) t_e ->
+                 let (n_f_list, n_scope, n_stmt) = compile_exp_r t_e t_f_list t_scope in
+                     (n_f_list, n_scope, n_stmt::stmts))
+            (f_list, scope, []) exps in
+        let assign value = (f_list1, scope1, Cish_ast.Exp(Cish_ast.Assign(result_var, value), dummy_pos)) in
+            match op with
+                | Plus   assign Cish_ast.Binop(
+                | Minus  (* subtract two ints *)
+                | Times  (* multiply two ints *)
+                | Div    (* divide two ints *)
+                | Cons   (* create a pair *)
+                | Fst    (* fetch the 1st component of a pair *)
+                | Snd    (* fetch the 2nd component of a pair *)
+                | Eq     (* compare two ints for equality *)
+                | Lt     (* compare two ints for inequality *)
+
   | Lambda(v, t_exp)  ->  (* TODO: create closure *)  
   | App(e1, e2)       -> raise Unimplemented (* TODO: call           *)
   | If(e1, e2, e3)    -> raise Unimplemented (* TODO: logical flow   *)
+
+
+
 
 let init_result (code : stmt) : stmt =
   (Let(result_name, (null, stub_pos), code), stub_pos)
