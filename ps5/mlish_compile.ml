c6e583c60fab2be9be516bcf852bf78fb0dc6656
Aaron Watanabe <awatanabe55@gmail.com>
2011-10-28 10:11:09 -0400

    Design for compiling mlish to scish

--- ps5/mlish_compile.ml
+++ ps5/mlish_compile.ml
@@ -2,4 +2,13 @@ module ML = Mlish_ast
 module S = Scish_ast
 
 exception ImplementMe
-let rec compile_exp ((e,_):ML.exp) : S.exp = raise ImplementMe
+let rec compile_exp ((e,_):ML.exp) : S.exp = 
+match e with
+    | ML.Var(v)
+    | ML.PrimApp(prim, es)
+    | ML.Fn(param, body)
+    | ML.App (func, arg)
+    | ML.If(e1, e2, e3)
+    | ML.Let(v, e1, e2)
+
+raise ImplementMe
