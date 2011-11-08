9942ddfff0776a41e6c5c34a95fc1638a7964a1f
Aaron Watanabe <awatanabe55@gmail.com>
2011-11-08 16:06:43 -0500

    Added monadize function in test file

--- ps6/optimize_tests.ml
+++ ps6/optimize_tests.ml
@@ -1,4 +1,15 @@
-open Monad
 open Monadic
+open Test_framework
 
-print_string "Hi";;
+exception TypeFailure
+
+
+let monadize (s: string) : Monad.exp =
+    let mlish_code = Ml_parse.program Ml_lex.lexer (Lexing.from_string s) in
+        (* Type check *)
+    let _ = Mlish_type_check.type_check_exp mlish_code in
+        Monadic.tomonadic (Mlish_compile.compile_exp mlish_code)
+
+let make_cfold_test =
+    mk_verbose_expect_test 
+
