ce02a5e26fd779cc926ab03633700c44a96e11fb
Aaron Watanabe <awatanabe55@gmail.com>
2011-11-19 10:15:41 -0500

    Added ps7 skeleton files

---
+++ ps7/cfg.ml
@@ -0,0 +1,38 @@
+open Cfg_ast
+exception Implement_Me
+exception FatalError
+
+(*******************************************************************)
+(* PS7 TODO:  interference graph construction *)
+
+(* an interference graph maps a variable x to the set of variables that
+ * y such that x and y are live at the same point in time.  It's up to
+ * you how you want to represent the graph.  I've just put in a dummy
+ * definition for now.  *)
+type interfere_graph = unit
+
+(* given a function (i.e., list of basic blocks), construct the
+ * interference graph for that function.  This will require that
+ * you build a dataflow analysis for calculating what set of variables
+ * are live-in and live-out for each program point. *)
+let build_interfere_graph (f : func) : interfere_graph = 
+    raise Implement_Me
+
+(*******************************************************************)
+(* PS8 TODO:  graph-coloring, coalescing register assignment *)
+(* You will need to build a mapping from variables to MIPS registers
+   using the ideas behind the graph-coloring register allocation
+   heuristics described in class.  This may involve spilling some
+   of the variables into memory, so be sure to adjust the prelude
+   of the function so that you allocate enough space on the stack
+   to store any spilled variables.  The output should be a CFG
+   function that doesn't use any variables (except for function
+   names.)
+*)
+let reg_alloc (f : func) : func = 
+    raise Implement_Me
+
+(* Finally, translate the ouptut of reg_alloc to Mips instructions *)
+let cfg_to_mips (f : func ) : Mips.inst list = 
+    raise Implement_Me
+
