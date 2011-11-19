9de2bf9451fa2d815f2841c0421c1ad869f044cd
Aaron Watanabe <awatanabe55@gmail.com>
2011-11-19 10:51:15 -0500

    Added graph file

---
+++ ps7/i_graph.ml
@@ -0,0 +1,6 @@
+
+(* an interference graph maps a variable x to the set of variables that
+ * y such that x and y are live at the same point in time.  It's up to
+ * you how you want to represent the graph.  I've just put in a dummy
+ * definition for now.  *)
+type interfere_graph = unit
