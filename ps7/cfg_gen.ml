1ea909f7bf40ebdb5448ed4f1144fb0ee24a1247
spartango@gmail.com <spartango@gmail.com>
2011-11-20 13:04:05 -0500

    fixed compile errors

--- ps7/cfg_gen.ml
+++ ps7/cfg_gen.ml
@@ -1,4 +1,5 @@
 open Io_types
+open Cfg_ast
 
 let get_rw (i: inst) : io_inst =
   { inst_read = ReadSet.empty; inst_write = WriteSet.empty; inst_in = InSet.empty; inst_out = OutSet.empty; inst_move = []; src_inst = i}
