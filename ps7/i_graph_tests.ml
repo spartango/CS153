a8e40d32bc02531bb9298057f9d262af9d6bf0f1
Aaron Watanabe <awatanabe55@gmail.com>
2011-11-29 21:39:34 -0500

    Added test code for i_graphs

--- ps7/i_graph_tests.ml
+++ ps7/i_graph_tests.ml
@@ -1,2 +1,15 @@
 open Pretty_print
 open Test_framework
+open I_graph
+open Cfg_gen
+
+let build_edge_set (v: var) (edge_vars: var list) : IGEdgeSet.t =
+    List.fold_left (fun edge_var set -> IGEdgeSet.add { left = edge_var; right = v}) IGEdgeSet.empty edge_vars
+
+let build_node (v: var) (interferes: var list) : ignode =
+    let node1 = new_ignode v in
+        ignode_set_edges (build_edge_set v interferes) node1
+
+let node1 = build_node "n1" ["n2"]
+let node2 = build_node "n2" ["n1"; "n3"]
+let node3 = build_node "n3" ["n2"]
