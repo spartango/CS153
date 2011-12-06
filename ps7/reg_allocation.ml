c0cfa848f655e3d14803165ca255c6479cedb8e5
Aaron Watanabe <awatanabe55@gmail.com>
2011-12-06 11:07:53 -0500

    Revised design of remove_edge and renamed remove_interfere. Implemented remove node

--- ps7/reg_allocation.ml
+++ ps7/reg_allocation.ml
@@ -1,3 +1,5 @@
+open Cfg_ast
+open Io_types
 open I_graph
 
 (* Helper functions *)
@@ -10,15 +12,18 @@ let igraph_map (f: ignode -> ignode) (graph: interfere_graph) : interfere_graph 
 let count_edges (node: ignode) : int =
     IGEdgeSet.cardinal node.edges
 
-(* Remove an edge from a node. For edge {left = l;right = r}, will also remove {left = r; right = l} *)
-let remove_edge (node: ignode) (edge: igedge) : ignode =
-    (* Creates the inverse of the edge *)
-    let rev_edge = { left = edge.right; right = edge.left } in
-    let new_edge_set = IGEdgeSet.remove edge (IGEdgeSet.remove rev_edge node.edges) in
-        (* Return node *)
+(* Unmarks interfering as interfering with node. *)
+(* Makes no change to node if does not already interfere with interfereing *)
+let remove_interfere (node: ignode) (interfering: var) : ignode =
+    (* Builds edge to remove *)
+    let edge = {interfere_var = interfering; node_var = node.name} in
+    let new_edge_set = IGEdgeSet.remove edge node.edges in
+        (* Return updated node *)
         ignode_set_edges new_edge_set node
 
 (* Takes a node out of the graph, removing edges to it *)
 let remove_node (node: ignode) (graph: interfere_graph) : interfere_graph =
+    (* Removes node from graph *)
     let updated_graph = IGNodeSet.remove node graph in 
-    (* Folds over a set of edges
+    (* Removes all remaining interference edges to that node in the graph *)
+        igraph_map (fun n -> remove_interfere n node.name) updated_graph
