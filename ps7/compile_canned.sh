01f47d3e537a1808a6852b652880b57e122828f5
spartango@gmail.com <spartango@gmail.com>
2011-12-19 00:39:46 -0500

    added test scripts

---
+++ ps7/compile_canned.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "[==========] Compiling Canned Cish Tests"
+
+rm -rf compiled_tests
+
+mkdir compiled_tests
+
+if [[ -e ps7_cfg	 ]]; then
+	for filename in `ls test/*.cish`; do
+		target=${filename%.cish}
+		output_file="compiled_tests/${target:5}.asm"
+
+		echo "\x1b\x5b1;36m[ COMPILE  ]\x1b\x5b0m ${filename:5}"
+		# Tags on the debug print stuff at the header of the program so we can actually test
+
+		# Compile
+		./ps7_cfg $filename > $output_file
+
+		# Tag on little bits to print out the results of the program
+		./inject.py $output_file > ${output_file%.asm}_test.asm
+
+	done 
+else 
+	echo "\x1b\x5b1;31m[  ERROR   ]\x1b\x5b0m: Compiler  not found"
+fi
+
+echo "[==========] Complete"
